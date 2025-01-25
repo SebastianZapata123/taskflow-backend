779650aa06807b25102073304cc6f9ab778c34f5
Sebastian Zapata <sebaszapata2022@gmail.com>
2025-01-24 19:12:21 -0500

    index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+require("dotenv").config(); // Cargar variables de entorno
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+// Middlewares para analizar JSON
+app.use(express.json());
+
+// Conexión a MongoDB
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Conectado a MongoDB 🚀"))
+  .catch((err) => {
+    console.error("Error al conectar a MongoDB:", err);
+    process.exit(1); // Salir del proceso si no se puede conectar a la base de datos
+  });
+
+// Puerto del servidor
+const PORT = process.env.PORT || 3000;
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
